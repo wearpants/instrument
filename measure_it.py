c8e4366500c6cae9b02fed8a0338d66c7ccf8761
Peter Fein <pete@wearpants.org>
2013-08-02 08:56:22 -0500

    mostly working, nice

--- measure_it.py
+++ measure_it.py
@@ -18,30 +18,28 @@ def measure(iterable, metric = print_metric, name = None):
     it = enumerate(iterable, 1) # count, element
     try:
         while True:
-            # time retrieving an element, and add to total elapsed
+            # time retrieving an element, and add to total elapsed. In a
+            # try/finally s.t. if next() throws an exception, we account for
+            # that time
             t = time()
             try:
                 count, x = next(it)
             finally:
-                # increment in a try/finally s.t. if next() throws an exception,
-                # we account for that time
                 total_time += time() - t
             yield x
     finally:
-        # we exit through here either with StopIteration or another exception
-        metric(name, 
-               count, total_time)
+        # underlying iterable is exhausted (StopIteration) or errored. Record
+        # the `metric` and allow exception to propogate
+        metric(name, count, total_time)
+
+def _measure_decorate(metric = print_metric, name = None):
+    def wrapper(func):        
+        def wrapped(*args, **kwargs):
+            iterable = func(*args, **kwargs)
+            return measure(iterable, metric, name if name is not None
+                           else func.__module__ + '.' +func.__name__)
         
-def measure_decorator(func, metric = print_metric, name = None):
-    """Wrap a generator function in `measure`
-
-    :arg function func: a function that produces a generator
-    :arg function metric: f(name, count, total_time)
-    :arg str name: a name for the stats. If None, derived from function name
-    """
-    def wrapper(*args, **kwargs):
-        iterable = func(*args, **kwargs)
-        return measure(iterable, metric, name if name is not None else
-                         func.__module__ + '.' +func.__name__)
-
+        return wrapped
     return wrapper
+
+measure.func = _measure_decorate
