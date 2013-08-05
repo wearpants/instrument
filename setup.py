9f1950db38e7d055c6ab5a1711fe48d74c491214
Peter Fein <pete@wearpants.org>
2013-08-05 13:54:41 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='measure_it',
+      version="0.1",
+      description='a Pythonic logger',
+      author='Pete Fein',
+      author_email='pete@wearpants.org',
+      url='http://github.com/wearpants/measure_it',
+      py_modules=['measure_it'],
+      license = "BSD",
+      classifiers = [
+      "Development Status :: 5 - Production/Stable",
+      "Intended Audience :: Developers",
+      "License :: OSI Approved :: BSD License",],
+      long_description=open('README.rst').read(),
+      )
