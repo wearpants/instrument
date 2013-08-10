b6dfd4c70609e99fb10acd0caa88e54d2626b462
Peter Fein <pete@wearpants.org>
2013-08-10 10:23:16 -0500

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='measure-it',
-      version="0.2",
+      version="0.2.1",
       description='time and count measurement for iterators',
       author='Pete Fein',
       author_email='pete@wearpants.org',
