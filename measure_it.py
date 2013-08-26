a861a28b40118c571461bb8c1b9325caf765b17e
Peter Fein <pete@wearpants.org>
2013-08-26 07:25:27 -0500

    stub running stats

--- measure_it.py
+++ measure_it.py
@@ -326,3 +326,31 @@ else:
         with statsd.pipeline() as pipe:
             pipe.incr(name, count)
             pipe.timing(name, int(round(1000 * elapsed)))  # milliseconds
+
+import stats
+
+class RunningStats(object):
+
+    stats = dict()
+    
+    def __init__(self, name):
+        self.name = name
+        self.points = 0
+        self.elapsed = stats.running_sum()
+        self.count = stats.running_sum()
+        self.last_elapsed = None
+        self.last_count = None
+        
+    def record(self, count, elapsed):
+        self.points += 1
+        self.last_elapsed = self.elapsed.send(elapsed)
+        self.last_count = self.count.send(count)
+        
+    @classmethod
+    def metric(cls, name, count, elapsed):
+        try:
+            self = cls.stats[name]
+        except KeyError:
+            self = cls.stats[name] = cls(name)
+        
+        self.record(count, elapsed)
