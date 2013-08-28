084a30bf7b693e7e0e087cc0355733bf7b219918
Peter Fein <pete@wearpants.org>
2013-08-28 01:05:33 -0500

    more csv

--- measure_it/csv.py
+++ measure_it/csv.py
@@ -8,8 +8,10 @@ import atexit
 import sys
 import csv
 
-class CSVMetric(object):
-    """Write metrics to individual CSV files
+__all__ = ['CSVDirMetric', 'CSVFileMetric']
+
+class CSVDirMetric(object):
+    """Write metrics to multiple CSV files
 
     Do not create instances of this class directly. Simply pass the
     classmethod :func:`metric` to a measurement function. Output using
@@ -18,7 +20,7 @@ class CSVMetric(object):
     Each metric consumes one open file and 32K of memory while running.
 
     :cvar dump_atexit: automatically call :func:`dump` when the interpreter exits. Defaults to True.
-    :cvar outdir: directory to save plots in. Defaults to `./mit_csv`.
+    :cvar outdir: directory to save CSV files in. Defaults to `./mit_csv`.
     """
     outdir = os.path.abspath("mit_csv")
     dump_atexit = True
@@ -28,13 +30,13 @@ class CSVMetric(object):
 
     def __init__(self, name):
         self.name = name
-        self.outfile = open(os.path.join(self.outdir, ".".join((self.name, 'csv'))),
+        self.fh = open(os.path.join(self.outdir, ".".join((self.name, 'csv'))),
                             'w', buffering=32768)
-        self.writer = csv.writer(self.outfile)
+        self.writer = csv.writer(self.fh)
 
     @classmethod
     def metric(cls, name, count, elapsed):
-        """A metric function that writes individual CSV files
+        """A metric function that writes multiple CSV files
 
         :arg str name: name of the metric
         :arg int count: number of elements
@@ -62,7 +64,7 @@ class CSVMetric(object):
 
     @classmethod
     def dump(cls):
-        """Output all recorded stats"""
+        """Output all recorded metrics"""
         with cls.lock:
             if not cls.instances: return
             if cls.dump_atexit and sys.version_info.major >= 3:
@@ -70,4 +72,54 @@ class CSVMetric(object):
                 atexit.unregister(cls.dump)
 
             for self in cls.instances.values():
-                self.outfile.close()
+                self.fh.close()
+
+class CSVFileMetric(object):
+    """Write metrics to a single CSV file
+
+    Pass the method :func:`metric` to a measurement function. Output using
+    :func:`dump`. These are the only public methods.
+
+    :ivar outfile: file to save to. Defaults to `./mit.csv`.
+    :ivar dump_atexit: automatically call :func:`dump` when the interpreter exits. Defaults to True.
+    """
+
+    def __init__(self, outfile="mit.csv", dump_atexit = True):
+        self.outfile = os.path.abspath(outfile)
+        if os.path.exists(self.outfile):
+            os.remove(self.outfile)
+
+        dirname = os.path.dirname(outfile)
+        if dirname:
+            os.makedirs(dirname, exist_ok=True)
+
+        self.dump_atexit = dump_atexit
+        if dump_atexit:
+            atexit.register(self.dump)
+
+        self.lock = threading.Lock()
+        self.fh = open(self.outfile, 'w', buffering=32768)
+        self.writer = csv.writer(self.fh)
+
+    def metric(self, name, count, elapsed):
+        """A metric function that writes a single CSV file
+
+        :arg str name: name of the metric
+        :arg int count: number of elements
+        :arg float elapsed: time in seconds
+        """
+
+        if name is None:
+            warnings.warn("Ignoring unnamed metric", stacklevel=3)
+            return
+
+        with self.lock:
+            self.writer.writerow((name, count, "%f"%elapsed))
+
+    def dump(self):
+        """Output all recorded metrics"""
+        with self.lock:
+            if self.dump_atexit and sys.version_info.major >= 3:
+                # python2.7 has no unregister function
+                atexit.unregister(self.dump)
+            self.fh.close()
