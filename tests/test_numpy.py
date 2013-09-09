e0ab8a63ad01c03d4413dec13ad0122c4b61742b
Peter Fein <pete@wearpants.org>
2013-09-09 18:30:22 -0500

    tests for numpy. fixes #1

--- tests/test_numpy.py
+++ tests/test_numpy.py
@@ -28,3 +28,26 @@ class StatsMetricTestCase(MeasureItTestCase):
         StatsMetric.dump()
         result = 'Name         Count Mean        Count Stddev        Elapsed Mean        Elapsed Stddev        \nalice          15.00               5.00               15.00                 5.00             \nbob            10.00               0.00               10.00                 0.00             \n'
         self.assertMultiLineEqual(StatsMetric.outfile.getvalue(), result)
+
+class PlotMetricTestCase(MeasureItTestCase):
+
+    def test_plot(self):
+        tmp = tempfile.mktemp()
+        self.addCleanup(shutil.rmtree, tmp)
+
+        PlotMetric.dump_atexit = False
+        PlotMetric.outdir = tmp
+
+        assert not os.path.exists(tmp)
+
+        list(measure_iter(math_is_hard(10), metric=PlotMetric.metric, name="alice"))
+        list(measure_iter(math_is_hard(20), metric=PlotMetric.metric, name="alice"))
+        list(measure_iter(math_is_hard(10), metric=PlotMetric.metric, name="bob"))
+
+        # unnamed metrics are dropped
+        list(measure_iter(math_is_hard(10), metric=PlotMetric.metric))
+
+        PlotMetric.dump()
+
+        # just test that files were created
+        self.assertItemsEqual(os.listdir(tmp), ['alice.png', 'bob.png'])
