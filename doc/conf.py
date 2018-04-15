40152b49b3486307141aebb89e1a3e30774883df
Pete Fein <pete@wearpants.org>
2018-04-15 14:27:39 -0400

    bump year

--- doc/conf.py
+++ doc/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'measure_it'
-copyright = u'2013, Pete Fein'
+copyright = u'2018, Pete Fein'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
